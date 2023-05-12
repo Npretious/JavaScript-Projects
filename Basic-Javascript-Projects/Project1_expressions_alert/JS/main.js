57f1c4ce94eee1a2ca56b69f98b85b3009e59ba6
Npretious <129784159+Npretious@users.noreply.github.com>
2023-05-12 13:32:40 -0600

    Pushing Project 1 from Javascript

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+//main.js
+
+//window.alert method
+window.alert("I love race cars!");
+
+//document.write method
+document.write("My favorite car is the Nissan Skyline R34!");
+
+//creating multiple variables
+var Sent1 = " Beginning of the string!";
+var Sent2 = " End of the string!";
+
+//concatenating the variables
+document.write(Sent1 + Sent2);
+
+//displaying an expression with the document.write method
+document.write(55 / 55);
