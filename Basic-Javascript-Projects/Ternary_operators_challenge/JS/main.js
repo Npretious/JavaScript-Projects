14dfb0bf8247e7b56a53d6e3ba46f355969adb6f
Npretious <129784159+Npretious@users.noreply.github.com>
2023-05-17 22:39:32 -0600

    Ternary _operators_challenge_push
    Pushing files containing JavaScript code regarding a school challenge

---
+++ Basic-Javascript-Projects/Ternary_operators_challenge/JS/main.js
@@ -0,0 +1,8 @@
+//main.js
+
+function Vote_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}
